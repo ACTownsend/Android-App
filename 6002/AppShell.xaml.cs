f5c41f6c22a937e393f69002c0e29b03c931fb45
Adam Townsend <townse42@uni.coventry.ac.uk>
2023-03-15 04:35:25 +0000

    Uploaded Base files for a .net MAUI app

---
+++ 6002/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace _6002;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ 6002/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace _6002;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
